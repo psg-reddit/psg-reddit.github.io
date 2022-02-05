a1e2c549fef8e7bd25a72f24004da4bbf9d78191
psg-reddit <70785583+psg-reddit@users.noreply.github.com>
2022-02-05 14:42:20 +1100

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var sources = [
     { name: 'Rubén Uría', type: 'journalist', tier: 1, workplace: 'beIN' },
     { name: 'TitisduPSG', type: 'media', tier: 1, workplace: null },
     { name: 'CulturePSG', type: 'media', tier: 1, workplace: null },
+    { name: 'beIN SPORTS', type: 'media', tier: 2, workplace: null },    
     // Medium tier source
     { name: 'Guillaume Maillard-Pacini', type: 'journalist', tier: 2, workplace: 'Eurosport' },
     { name: 'Loïc Tanzi', type: 'journalist', tier: 2, workplace: 'RMC' },
@@ -32,7 +33,6 @@ var sources = [
     { name: 'Maxime Dupuis', type: 'journalist', tier: 2, workplace: 'Eurosport' },
     { name: 'Mohamed Bouhafsi', type: 'journalist', tier: 2, workplace: 'RMC' },
     { name: 'LeParisien', type: 'media', tier: 2, workplace: null },
-    { name: 'beIN SPORTS', type: 'media', tier: 2, workplace: null },
     { name: 'RMC', type: 'media', tier: 2, workplace: null },
     { name: 'Canal Supporters', type: 'media', tier: 2, workplace: null },
     { name: 'L’Équipe', type: 'media', tier: 2, workplace: null },
